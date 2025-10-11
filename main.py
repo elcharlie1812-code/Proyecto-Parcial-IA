2774a01b9b0f4c914fb74f2f6203a599d0c80469
elcharlie1812-code <elcharlie1812@gmail.com>
2025-10-11 16:48:48 -0400

    from

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+from scrips.Jugador import Jugador
+from scrips.Enemigo import Enemigo
+from scrips.Albordecomportamiento import Albordecomportamiento
+
+
+
+pygame.init()   
+from scrips.Config import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, TILE_SIZE
+
+
